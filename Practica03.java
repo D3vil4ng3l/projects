6448de2c19de9efb0eafd2f6715d6e6f9f463ba2
D3vil4ng3l <92679821+D3vil4ng3l@users.noreply.github.com>
2021-10-19 14:11:48 +0200

    Update Practica03.java

--- Practica03.java
+++ Practica03.java
@@ -29,12 +29,19 @@ public class Practica03 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        
+        // Creamos las 3 Box dos verticales y un horizontal que contendrá a las otras
         VBox left = new VBox();
         VBox right = new VBox();
+        
+        /* Uno de los constructores permite introducir por parametro 
+        los nodos hijos */
         HBox root = new HBox(left, right);
 
+        // Definimos el tamaño de la escena (Pestaña)
         Scene scene = new Scene(root, 500, 300);
 
+        // Definimos cada uno de los elementos del Login
         primaryStage.setTitle("VBox Layout");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -57,24 +64,32 @@ public class Practica03 extends Application {
         btnLogin.setMaxWidth(150);
         btnLogin.setCursor(Cursor.HAND);
 
+        // Definimos los hijos del VBox left
         left.getChildren().addAll(labelTitle, labelUser, textUser,
                 labelPassword, textPass, btnLogin);
         left.setAlignment(Pos.CENTER);
 
+        // Definimos el margen de los todos los elementos
+        VBox.setMargin(labelTitle, new Insets(5, 0, 0, 20));
         VBox.setMargin(labelUser, new Insets(5, 0, 0, 20));
         VBox.setMargin(labelPassword, new Insets(5, 0, 0, 20));
         VBox.setMargin(btnLogin, new Insets(5, 0, 0, 20));
         VBox.setMargin(textUser, new Insets(5, 0, 0, 20));
         VBox.setMargin(textPass, new Insets(5, 0, 0, 20));
         
+        // Creamos el objeto image, poendo la ruta de la imagen como parametro
         Image image = new Image(getClass().getResourceAsStream("/imagenes/imagen.jpg"));
         ImageView imageLogo = new ImageView(image);
+        
+        // Ajustamos el tamaño de la imagen
         imageLogo.setFitHeight(200);
         imageLogo.setFitWidth(300);
+        
+        // Ajustamos el margen de la imagen
         VBox.setMargin(imageLogo, new Insets(0, 0, 0, 30));
         
-        
-        right.getChildren().addAll(imageLogo);
+        // Añadimos la imagen a nuestro nodo right
+        right.getChildren().add(imageLogo);
         right.setAlignment(Pos.CENTER);
         
     }
